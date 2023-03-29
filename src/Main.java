37cd27f376976fb21d07d611e7517618101e8de4
ottorion <yigitbasakcharlotte@gmail.com>
2023-03-29 19:18:06 -0400

    the basic structure of the car is ready

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+BasicStructure audi=new BasicStructure();
+    audi.model="A3";
+    audi.power="v8";
+    audi.speed="180";
+    }
+}
